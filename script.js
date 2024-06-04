5b9bd779d0dd5f6a161100103a2b079bae5138b0
Pedro Henrique Oliveira Freitas <pedrofreitas@ses.int>
2024-06-04 11:34:37 -0400

    Projeto Serenatto

---
+++ script.js
@@ -0,0 +1,7 @@
+let inputCheck = document.querySelector('#modo-noturno');
+let elemento = document.querySelector('body');
+
+inputCheck.addEventListener('click', () => {
+    let modo = inputCheck.checked ? 'dark' : 'light';
+    elemento.setAttribute('data-bs-theme', modo);
+})
